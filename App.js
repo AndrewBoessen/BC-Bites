a38d39f7a5bccebfbed653d69545ea99cc0941ff
Andrew Boessen <boessena@bc.edu>
2022-11-24 00:13:55 -0500

    home page done

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider, Button, createTheme } from '@rneui/themed';
+import HomePage from './pages/Home.js'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const theme = createTheme({
+  components: {
+    Button: {
+      raised: true,
+    },
   },
 });
+
+// Your App
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <HomePage/>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ pages/Menus.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, Skeleton, Image, Header } from '@rneui/themed';
+import { View, StyleSheet} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const MainPage = (props) => {
+    return(
+        <SafeAreaProvider>
+
+        </SafeAreaProvider>
+    );
+}
+
+
+const stlyes = StyleSheet.create({
+
+});
+
+export default MainPage;
