845627a642818989b437f8d3caef2c6bc678e0c5
Andrew Boessen <boessena@bc.edu>
2022-11-26 18:13:51 -0500

    a lot of stuf idk

--- App.js
+++ App.js
@@ -1,11 +1,83 @@
-import { ThemeProvider, Button, createTheme } from '@rneui/themed';
-import HomePage from './pages/Home.js'
+import { ThemeProvider, createTheme } from '@rneui/themed';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, cardStyle } from '@react-navigation/native-stack';
+import MainPage from './pages/Menus'
+
+const Stack = createNativeStackNavigator();
+
+const BCRed = '#8a100b';
+const BCGold = '#b29d6c';
 
 const theme = createTheme({
+  colors: {
+    primary: BCRed,
+    secondary: BCGold,
+  },
   components: {
+    View: {
+      style: {
+        flex: 1,
+      }
+    },
     Button: {
       raised: true,
     },
+    Header: {alignItems: 'center',   width: '100%',
+        paddingVertical: 10,
+    },
+    Text: {
+      style:{
+        fontFamily: Platform.OS === 'ios' ? 'Menlo-Bold' : '',
+      },
+      h3Style: {
+        color: 'white',
+        fontSize: 22,
+      },
+      h4Style: {
+        color: 'black',
+        fontSize: 12,
+      }
+    },
+    Icon: {
+      color: 'white',
+    },
+    Chip: {
+      radius: 15,
+      containerStyle: {
+        marginVertical: 10,
+        marginHorizontal: 5,
+      },
+      buttonStyle: {
+        height: 35,
+        width: 100,
+      },
+      titleStyle: {
+        fontFamily: Platform.OS === 'ios' ? 'Menlo-Bold' : '',
+      }
+    },
+    Divider: {
+      color: BCRed,
+      width: 3,
+      orientation: 'horizontal',
+    },
+    Card: {
+      containerStyle: {
+        borderRadius: 15,
+        flex: 1,
+      },
+    },
+    CardTitle: {
+      style:{
+        fontFamily: Platform.OS === 'ios' ? 'Menlo-Bold' : '',
+      },
+    },
+    Image: {
+      containerStyle:{
+        width: 80,
+        height: 80,
+        marginHorizontal: 10,
+      }
+    }
   },
 });
 
@@ -13,7 +85,13 @@ const theme = createTheme({
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <HomePage/>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{headerShown : false}}
+        >
+          <Stack.Screen name = "Menus" component = {MainPage} options = {{animation : 'fade'}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </ThemeProvider>
   );
 };
--- pages/Menus.js
+++ pages/Menus.js
@@ -1,20 +1,68 @@
-import React from 'react';
-import { Text, Skeleton, Image, Header } from '@rneui/themed';
-import { View, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, Skeleton, Image, Header, Icon, Chip, Divider, useTheme, Card } from '@rneui/themed';
+import { View, ScrollView } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { LinearGradient } from 'expo-linear-gradient'
 
-const MainPage = (props) => {
+const DiningHallChip = (props) => {
+    const [selected, setSelected] = useState(props.startSelcted);
+
     return(
-        <SafeAreaProvider>
-
-        </SafeAreaProvider>
+        <Chip 
+            title = {props.Name} 
+            type = {selected ? 'solid' : 'outline'}
+            onPress = {() =>{
+                setSelected(!selected);
+            }}
+        />
     );
 }
 
+const MenuItem = (props) => {
+    return(
+        <Card>
+            <Card.Title>{props.name}</Card.Title>
+            <Card.Divider/>
+            <View style = {{flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
+                <Image
+                    source = {{uri : props.img}}
+                    PlaceholderContent = {<Skeleton animation = "pulse" width={80} height={80}/>}
+                />
+                <Text h4>Dining Hall: {props.hall}</Text>
+            </View>
+        </Card>
+    );
+}
 
-const stlyes = StyleSheet.create({
-
-});
+const MainPage = (props) => {
+    const {theme, updateTheme} = useTheme();
+    return(
+        <SafeAreaProvider>
+            <View>
+                <View>
+                    <Header
+                        leftComponent= {<Icon name = 'menu'/>}
+                        centerComponent = {<Text h3>BC-Bites</Text>}
+                    />
+                </View>
+                <View style = {{flexDirection: 'row', justifyContent: 'center'}}>
+                    <DiningHallChip Name = {"Lower"} startSelcted/>
+                    <DiningHallChip Name = {"Carney's"}/>
+                    <DiningHallChip Name = {"Stuart"}/>
+                </View>
+                <Divider/>
+                <ScrollView>
+                    <MenuItem name = {"Chicken & 2 Sides"} img = {"https://via.placeholder.com/80"} hall = {"lower"}/>
+                    <MenuItem name = {"Chicken & 2 Sides"} img = {"https://via.placeholder.com/80"} hall = {"lower"}/>
+                    <MenuItem name = {"Chicken & 2 Sides"} img = {"https://via.placeholder.com/80"} hall = {"lower"}/>
+                    <MenuItem name = {"Chicken & 2 Sides"} img = {"https://via.placeholder.com/80"} hall = {"lower"}/>
+                    <MenuItem name = {"Chicken & 2 Sides"} img = {"https://via.placeholder.com/80"} hall = {"lower"}/>
+                    <MenuItem name = {"Chicken & 2 Sides"} img = {"https://via.placeholder.com/80"} hall = {"lower"}/>
+                </ScrollView>
+            </View>
+        </SafeAreaProvider>
+    );
+}
 
 export default MainPage;
